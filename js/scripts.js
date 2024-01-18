7231c96ce62af02d9ab2d05b39becdbfc017c6c6
omishe <omishe@hotmail.com>
2024-01-17 18:53:02 -0600

    Little Practice of Web

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+const btn1 = document.querySelector( "#btn1" );
+
+btn1.addEventListener( "click", ( event ) => { 
+    alert( "Toque el botón: " + event.target.id );
+} );
+
+const btn2 = document.querySelector( "#btn2" );
+
+btn2.addEventListener( "click", ( event ) => { 
+    alert( "Toque el botón: " + event.target.id );
+} );
+
+const btn3 = document.querySelector( "#btn3" );
+
+btn3.addEventListener( "click", ( event ) => { 
+    alert( "Toque el botón: " + event.target.id );
+} );
